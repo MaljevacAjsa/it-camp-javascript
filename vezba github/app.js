981a021fe71dce150477cbe4d49f3c8891d5f4bc
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2024-07-22 19:01:12 +0200

    zadatak preko for petlje

--- vezba github/app.js
+++ vezba github/app.js
@@ -1,7 +1,13 @@
-const niz = [1, 2, 3, 4, 5, 8];
+const niz = [1, 2, 3, 4, 5];
 
-const rezultat = niz.reduce(function (akumulator, vrednost) {
-  return akumulator + vrednost / niz.length;
-}, 0);
+// const rezultat = niz.reduce(function (akumulator, vrednost) {
+//   return akumulator + vrednost / niz.length;
+// }, 0);
 
-console.log(rezultat);
+let rezultat = 0;
+
+for (i = 0; i <= niz.length; i++) {
+  rezultat = rezultat + i;
+}
+
+console.log(rezultat / niz.length);
