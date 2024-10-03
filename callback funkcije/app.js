74b8678457d43982ac0edad57bd48abf075f2770
Maljevac Ajsa <ajsamaljevac265@gmail.com>
2024-10-03 15:04:37 +0200

    callback funcions

--- callback funkcije/app.js
+++ callback funkcije/app.js
@@ -9,3 +9,41 @@ function prikaziRez(rezultat) {
 }
 
 mnozenje(5, prikaziRez);
+
+//////////////////////////////////////////////////////
+
+function sabiranje(broj, funkcija) {
+  const rezultat = broj + 3;
+  funkcija(rezultat);
+}
+
+function rezz(rezultat) {
+  console.log(`Ovo je rezultat :${rezultat}`);
+}
+
+sabiranje(5, rezz);
+
+//////////////////////////////////////////////////////
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+      if (!success) {
+        const data = { id: 1, ime: "Neko" };
+        resolve(data);
+      } else {
+        reject("greska");
+      }
+    }, 2000);
+  });
+}
+
+fetchData()
+  .then((data) => console.log("podatci su uspesno prikazani", data))
+  .catch((error) => {
+    console.log("neuspesno prikazivanje podataka", error);
+  })
+  .finally(() => {
+    console.log("kraj");
+  });
